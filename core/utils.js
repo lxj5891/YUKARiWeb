09f3f382d12dcd32530057dfc1edaed61435a8c5
jg_wang <jg_wang@dreamarts.co.jp>
2013-09-16 16:47:47 +0800

    权限check

---
+++ core/utils.js
@@ -0,0 +1,27 @@
+exports.isCommonUser = function(user_){//普通用户
+  return user_.type === 0;
+}
+
+exports.isAdmin = function(user_){//顾客管理员
+  return user_.type === 1;
+}
+
+exports.isSystemAdmin = function(user_){//DA 管理员
+  return user_.type === 2;
+}
+
+exports.isSuperAdmin = function(user_){// dev
+  return user_.type === 3;
+}
+
+exports.hasContentPermit = function(user_){
+  return user_.authority.contents === 1;
+}
+
+exports.hasNoticePermit = function(user_){
+  return user_.authority.notice === 1;
+}
+
+exports.hasApprovePermit = function(user_){
+  return user_.authority.approve === 1;
+}
