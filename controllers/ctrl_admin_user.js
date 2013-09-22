73c3e7640d21c90dcd7c0db2820b8dcc897f1234
slsay <shengli1222@163.com>
2013-09-22 13:10:37 +0800

    临时提交

--- controllers/ctrl_admin_user.js
+++ controllers/ctrl_admin_user.js
@@ -3,6 +3,7 @@ var json = lib.core.json
   , crl_user =lib.ctrl.user
   , _ = require('underscore')
   , sync     = require('async')
+  , error     = lib.core.errors
   , company = require('../modules/mod_company');
 //yukri
 exports.adminlist = function(uid_,callback) {
@@ -35,7 +36,7 @@ exports.adminlist = function(uid_,callback) {
 exports.adminsearchOne = function(code_, userid_,callback_) {
   crl_user.searchOneByDBName(code_, userid_, function(err, result){
     if (err) {
-      return callback_(new error.InternalServer(err));
+      return callback_(err);
     }
     company.getByCode(result.companycode,function(err,comps){
       if(err) {
@@ -54,20 +55,12 @@ exports.add = function(uid_, data_, callback_) {
   var dbName = data_.companycode;
   data_.type = 0;
   crl_user.addByDBName(dbName,uid_, data_, function(err, result) {
-    if (err) {
-      return callback_(new error.InternalServer(err));
-    } else {
-      return callback_(err, result);
-    }
+    return callback_(err, result);
   });
 };
 exports.update = function(uid_, data_, callback_) {
   var dbName = data_.companycode;
   crl_user.updateByDBName(dbName,uid_, data_, function(err, result) {
-    if (err) {
-      return callback_(new error.InternalServer(err));
-    } else {
-      return callback_(err, result);
-    }
+     return callback_(err, result);
   });
 };
