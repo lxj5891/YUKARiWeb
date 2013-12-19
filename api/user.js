188aecad1e5a0bcdde93f5b7660b8c08cae8df09
wuqinlong <wuqinlong1129@163.com>
2013-12-19 21:08:20 +0800

    把simplelogin从app层下降到control层

--- api/user.js
+++ api/user.js
@@ -1,8 +1,6 @@
 var json      = smart.framework.response
   , errors    = smart.framework.errors
-  , log       = smart.framework.log
-  , auth     = smart.framework.auth
-  , response    = smart.framework.response
+
   , user      = require('../controllers/ctrl_user')
   , util      = require('../core/utils');
 
@@ -36,54 +34,7 @@ exports.searchOne = function(req_, res_) {
   });
 };
 //////edit by zhaobing//////////////////////////////////////////////////////////////////////////////////////////
+///remove to controllers
 exports.simpleLogin = function(req, res){
-  var deivceId = req.query.deviceid;
-  var logined = function() {
-    if (req.session.user.type == 1) // 重新设定管理员画面
-      req.query.home = "/admin";
-    //
-  };
-  var path = req.query.path; // 公司ID, Web登陆用
-  var code = req.query.code; // 公司Code，iPad登陆用
-  // 登陆到公司的DB进行Login
-  if(path) {
-    ctrl_company.getByPath(path, function(err, comp){
-      if(err)
-        return errorsExt.sendJSON(res, err);
-      if(!comp)
-        return errorsExt.sendJSON(res, errorsExt.NoCompanyID);
-      // var companyDB = comp.code;
-      req.query.companycode= comp.code;
-    })
-    // iPad登陆
-  } else if(code) {
-    ctrl_company.getByCode(code, function(err, comp){
-      if(err)
-        return errorsExt.sendJSON(res, err);
-      if(!comp)
-        return errorsExt.sendJSON(res, errorsExt.NoCompanyCode);
-      var companyDB = comp.code;
-      req.query.companycode = code;
-    });
-  }
-
-////////////////////////////////////////////////////////////////////////
-  log.debug("user name: " + req.query.name);
-
-  // パスワードのsha256文字列を取得する
-  req.query.password = auth.sha256(req.query.password);
-
-  // 認証処理
-  auth.simpleLogin(req, res, function(err, result) {
-
-    if (err) {
-      log.error(err, undefined);
-      log.audit("login failed.", req.query.name);
-    } else {
-      log.audit("login succeed.", result._id);
-    }
-
-    response.send(res, err, result);
-  });
+  user.simpleLogin(req,res);
 };
-//////////////////////////////////////////////////////////////////////////////////////////
--- controllers/ctrl_user.js
+++ controllers/ctrl_user.js
@@ -4,7 +4,62 @@
   var util      = smart.framework.util
   , _         = smart.util.underscore
   , sync      = smart.util.async
+  , response    = smart.framework.response
+  , auth     = smart.framework.auth
+  , log       = smart.framework.log
   , company   = require('../modules/mod_company');
+
+
+// added by wuql from api/user edit by zhaobing
+exports.simpleLogin = function(req, res){
+  var deivceId = req.query.deviceid;
+  var logined = function() {
+    if (req.session.user.type == 1) // 重新设定管理员画面
+      req.query.home = "/admin";
+  };
+  var path = req.query.path; // 公司ID, Web登陆用
+  var code = req.query.code; // 公司Code，iPad登陆用
+  // 登陆到公司的DB进行Login
+  if(path) {
+    ctrl_company.getByPath(path, function(err, comp){
+      if(err)
+        return errorsExt.sendJSON(res, err);
+      if(!comp)
+        return errorsExt.sendJSON(res, errorsExt.NoCompanyID);
+      // var companyDB = comp.code;
+      req.query.companycode= comp.code;
+    })
+    // iPad登陆
+  } else if(code) {
+    ctrl_company.getByCode(code, function(err, comp){
+      if(err)
+        return errorsExt.sendJSON(res, err);
+      if(!comp)
+        return errorsExt.sendJSON(res, errorsExt.NoCompanyCode);
+//      var companyDB = comp.code;
+      req.query.companycode = code;
+    });
+  }
+
+  log.debug("user name: " + req.query.name);
+
+  // パスワードのsha256文字列を取得する
+  req.query.password = auth.sha256(req.query.password);
+
+  // 認証処理
+  auth.simpleLogin(req, res, function(err, result) {
+
+    if (err) {
+      log.error(err, undefined);
+      log.audit("login failed.", req.query.name);
+    } else {
+      log.audit("login succeed.", result._id);
+    }
+
+    response.send(res, err, result);
+  });
+};
+
 
 //yukri
 exports.listByDBName = function(dbName_,start_, limit_, keyword_, callback_) {
